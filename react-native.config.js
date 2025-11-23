16628ba49fd176d1c0661ee74389cc1ad3d82796
Punkajj Daas <baadhshah@gmail.com>
2025-11-23 20:26:55 +0530

    yml

---
+++ react-native.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  project: {
+    android: {
+      packageName: 'com.maplocation',
+      sourceDir: './android',
+      appName: 'app',
+    },
+    ios: {
+      project: './ios/MapLocation.xcworkspace',
+    },
+  },
+};
+
